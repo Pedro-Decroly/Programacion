0046bbc902daa138c28620fec73050a1c36e9b60
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2024-10-09 08:48:52 +0200

    no message

---
+++ UD2_Actividad_03_PedroArza/UD2_Actividad_03/src/App.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+//Realiza un programa que dada una cantidad de euros que el usuario introduce por teclado
+
+
+
+
+
+
+//2 Realiza un programa que muestre un menú de opciones
+        String opcion ="";
+        Scanner reader = new Scanner (System.in);
+        do { 
+            System.out.println("1º-Sumar");
+            System.out.println("2º-Restar");
+            System.out.println("3º-Multiplicar");
+            System.out.println("4º-Dividir");
+            System.out.println("5º-Salir");
+            
+            opcion = reader.nextLine();
+        } while (opcion !="5");
+    }
+}
