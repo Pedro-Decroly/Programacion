be6cd4e1866fc393ee7b20c533083fc2ba09f914
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2024-10-14 09:58:48 +0200

    Apartado 1 terminado

--- UD2_Actividad_03_PedroArza/UD2_Actividad_03/src/App.java
+++ UD2_Actividad_03_PedroArza/UD2_Actividad_03/src/App.java
@@ -7,7 +7,26 @@ public class App {
 
 //1-Realiza un programa que dada una cantidad de euros que el usuario introduce por teclado
 
+        Scanner reader = new Scanner(System.in);
+        int[] billetes = {500, 200, 100, 50, 20, 10, 5};
 
+        System.out.print("Introduce una cantidad en euros (múltiplo de 5): ");
+        int cantidad = reader.nextInt();
+
+        if (cantidad % 5 != 0) {
+            System.out.println("Error: La cantidad debe ser múltiplo de 5.");
+            return;
+        }
+
+        System.out.println("Para " + cantidad + " euros, necesitas:");
+
+        for (int billete : billetes) {
+            if (cantidad >= billete) {
+                int numBilletes = cantidad / billete;
+                System.out.println(numBilletes + " billete(s) de " + billete + " euros");
+                cantidad %= billete;
+            }
+        }
 
 
 
@@ -15,7 +34,7 @@ public class App {
 //2-Realiza un programa que muestre un menú de opciones
 
         String opcion ="";
-        Scanner reader = new Scanner (System.in);
+        Scanner reader1 = new Scanner (System.in);
         
         do {
             System.out.println("1-Suma");
@@ -23,49 +42,49 @@ public class App {
             System.out.println("3-Multiplicar");
             System.out.println("4-Dividir");
             System.out.println("5-Salir");
-            opcion = reader.nextLine();
+            opcion = reader1.nextLine();
 
            
             switch(opcion)
             {
                 case "1":
-                    reader = new Scanner(System.in);
+                    reader1 = new Scanner(System.in);
                         System.out.println("INTRODUCE UN NUMERO:");
-                            double numero1 = reader.nextDouble();
+                            double numero1 = reader1.nextDouble();
 				
 			        System.out.println("INTRODUCE UN NUMERO:");
-                        double numero2 = reader.nextDouble();
+                        double numero2 = reader1.nextDouble();
                             System.out.println("La Suma es:" + (numero1 + numero2));
                     
                 break;
                 
                 case "2":
-                    reader = new Scanner(System.in);
+                    reader1 = new Scanner(System.in);
                         System.out.println("INTRODUCE UN NUMERO:");
-				            double numero3 = reader.nextDouble();
+				            double numero3 = reader1.nextDouble();
 				
 			        System.out.println("INTRODUCE UN NUMERO:");
-                        double numero4 = reader.nextDouble();
+                        double numero4 = reader1.nextDouble();
                             System.out.println("La Resta es:" + (numero3 - numero4));
                 break;
                 
                 case "3":
-                    reader = new Scanner(System.in);
+                    reader1 = new Scanner(System.in);
                         System.out.println("INTRODUCE UN NUMERO:");
-                            double numero5 = reader.nextDouble();
+                            double numero5 = reader1.nextDouble();
 				
 			        System.out.println("INTRODUCE UN NUMERO:");
-                        double numero6 = reader.nextDouble();
+                        double numero6 = reader1.nextDouble();
                             System.out.println("El Producto es:" + (numero5 * numero6));
                 break;
 
                 case "4":
-                    reader = new Scanner(System.in);
+                    reader1 = new Scanner(System.in);
                         System.out.println("INTRODUCE UN NUMERO:");
-				             double numero7 = reader.nextDouble();
+				             double numero7 = reader1.nextDouble();
 				
 			        System.out.println("INTRODUCE UN NUMERO:");
-                        double numero8 = reader.nextDouble();
+                        double numero8 = reader1.nextDouble();
                             System.out.println("El Producto es:" + (numero7 / numero8));
                 break;
 
