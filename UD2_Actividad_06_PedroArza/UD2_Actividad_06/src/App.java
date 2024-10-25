23d7ab2edcdcc9dc2b1079465520a3af992c3613
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2024-10-25 09:05:36 +0200

    Actividad 6

--- UD2_Actividad_06_PedroArza/UD2_Actividad_06/src/App.java
+++ UD2_Actividad_06_PedroArza/UD2_Actividad_06/src/App.java
@@ -24,28 +24,187 @@ public class App {
 
         System.out.println("la matriz es" + miMatriz1);
     
-    //Actividad3
-        
-        Scanner in = new Scanner(System.in);
+    //EJERCICIO 3
+		 
+		 Scanner reader = new Scanner(System.in);
 
-        int N = in.nextInt();
-        
-        int M = in.nextInt();
-        
-        int [] [] miMatriz2 = new int [N] [M];
+		 
+		 System.out.println("introduzca el valor de n: ");
+		 
+		 int n = reader.nextInt();
+		 
+		 System.out.println("introduzca el valor de m: ");
+		 
+		 int m = reader.nextInt();
+		 
+		 int [][] matriz2 = new int [n][m];
+		 
+		 for (int i = 0; i < n; i++) {
+			 
+			 for (int j = 0; j < m; j++) {
+				 
+				 System.out.println("Valor en posición [" + i + "][" + j + "]: ");
+				 
+				 matriz2[i][j] = reader.nextInt();
+				 
+			 }
+			 
+		 }
+		 
+		 int mayores = 0;
+				 
+		 int menores = 0;
+		 
+		 int iguales = 0;
+		 
+		 for (int i = 0; i < n; i++) {
+			 
+			 for (int j = 0; j < m; j++) {
+				 
+	             if (matriz2[i][j] > 0) {
+	            	 
+	                 mayores++;
+	                 
+	             } else if (matriz2[i][j] < 0) {
+	            	 
+	                 menores++;
+	                 
+	             } else {
+	            	 
+	                 iguales++;
+	                 
+	             }
+	         }
+		 }
+		 
+		System.out.println("resultados: "); 
+		System.out.println("mayores que cero: " + mayores);
+		System.out.println("menores que cero: " + menores);
+		System.out.println("iguales que cero: " + iguales);
+		 
+		 
+		
+	//EJERCICIO 4
+		
+		 int alumnos = 4;
+	     int asignaturas = 5;
 
-        
-        System.out.println("Intruduce un valor a N");
-        System.out.println("Intruduce un valor a M");
+	     
+	     double[][] notas = new double[alumnos][asignaturas];
 
-        
-            
-            for (int i = 0; i < miMatriz2.length; i++) {
-                System.out.println(miMatriz2[i]);
-                    }
+	     
+	     for (int i = 0; i < alumnos; i++) {
+	    	 
+	         System.out.println("introduce las notas del Alumno " + (i + 1) + ":");
+	         
+	         for (int j = 0; j < asignaturas; j++) {
+	        	 
+	             System.out.print("nota de la asignatura " + (j + 1) + ": ");
+	             
+	             notas[i][j] = reader.nextDouble();
+	             
+	         }
+	     }
 
-            System.out.println("la matriz es" + miMatriz2);
-        
+	    
+	     for (int i = 0; i < alumnos; i++) {
+	    	 
+	         double suma = 0;
+	         double notaMin = notas[i][0];
+	         double notaMax = notas[i][0];
 
+	         for (int j = 0; j < asignaturas; j++) {
+	        	 
+	             double notaActual = notas[i][j];
+	             
+	             suma += notaActual;
+	             
+	             
+	             if (notaActual < notaMin) {
+	            	 
+	                 notaMin = notaActual;
+	                 
+	             }
+	             if (notaActual > notaMax) {
+	            	 
+	                 notaMax = notaActual;
+	                 
+	             }
+	         }
+
+	         
+	         double media = suma / asignaturas;
+
+	         
+	         System.out.println("alumno " + (i + 1) + ":");
+	         System.out.println("nota minima: " + notaMin);
+	         System.out.println("nota maxima: " + notaMax);
+	         System.out.println("nota media: " + media);
+	     }
+
+	//EJERCICIO 5
+	  
+	     System.out.print("introduce numero de personas: ");
+	     
+	     int numeroPersonas = reader.nextInt();
+
+	     
+	     double[][] datos = new double[numeroPersonas][2]; 
+
+	     
+	     double sumaHombres = 0, sumaMujeres = 0;
+	     
+	     int contHombres = 0, contMujeres = 0;
+
+	     
+	     for (int i = 0; i < numeroPersonas; i++) {
+	    	 
+	         System.out.println("\npersona " + (i + 1) + ":");
+	         
+	         System.out.print("introduce el genero 0 hombre, 1 mujer: ");
+	         
+	         int genero = reader.nextInt();
+
+	         
+	         while (genero != 0 && genero != 1) {
+	        	 
+	             System.out.print("introduce un genero valido: ");
+	             
+	             genero = reader.nextInt();
+	             
+	         }
+
+	         System.out.print("introduce el sueldo: ");
+	         double sueldo = reader.nextDouble();
+
+	         
+	         datos[i][0] = genero;
+	         datos[i][1] = sueldo;
+
+	         
+	         if (genero == 0) {
+	        	 
+	             sumaHombres += sueldo;
+	             
+	             contHombres++;
+	             
+	         } else if (genero == 1) {
+	        	 
+	             sumaMujeres += sueldo;
+	             
+	             contMujeres++;
+	             
+	         }
+	     }
+
+
+	     double mediaHombres = (contHombres > 0) ? sumaHombres / contHombres : 0;
+	     double mediaMujeres = (contMujeres > 0) ? sumaMujeres / contMujeres : 0;
+
+
+	     
+	     System.out.println("sueldo medio hombres: " + mediaHombres);
+	     System.out.println("sueldo medio mujeres: " + mediaMujeres);
+		
     }
 }
