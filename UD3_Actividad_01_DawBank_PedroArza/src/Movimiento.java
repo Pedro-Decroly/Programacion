ce45324b4967d05964de15d1508cf621b7a610d9
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2024-11-11 12:53:46 +0100

    Actividad1

--- UD3_Actividad_01_DawBank_PedroArza/src/Movimiento.java
+++ UD3_Actividad_01_DawBank_PedroArza/src/Movimiento.java
@@ -1,3 +1,46 @@
-public class Movimiento {
 
+import java.time.LocalDateTime;
+
+//Calses de mis movimientos 
+    public class Movimiento {
+
+        private static  int contador = 0;
+        private int id;
+        private String fecha;
+        private String tipo;
+        private double cantidad;
+
+//Constructor
+
+    public Movimiento(String tipo, double cantidad){
+
+        this.id = contador;
+        this.contador ++;
+        this.fecha = LocalDateTime.now().toString();
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+    }
+
+
+//Getters y Setters
+
+    public int getId(){
+        return this.id;
+    }
+    public String getFecha(){
+        return this.fecha;
+    }
+    public String getTipo(){
+        return this.tipo;
+    }
+    public Double getCantidad(){
+        return this.cantidad;
+    }
+    public void setCantidad(int cantidad){
+        if(cantidad > 0){
+            this.cantidad = cantidad;
+        }else{
+            System.out.println("El valor de la cantidad tiene que ser positivo");
+        }
+    }
 }
