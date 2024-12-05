df377b9cb80c63777236b87022c40c637d37dfe0
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2024-12-05 11:36:23 +0100

    no message

--- UD3_Actividad_02_VideoDaw_PEDROARZA/UD3_Actividad_02_VideoDaw/src/Cliente.java
+++ UD3_Actividad_02_VideoDaw_PEDROARZA/UD3_Actividad_02_VideoDaw/src/Cliente.java
@@ -3,13 +3,16 @@ import java.time.LocalDate;
 public class Cliente {
     public static int contador = 0;
 //Atributos Del Cliente
+
     private String DNI;
+
     private String Nombre;
+
     private int NumSocio;
+
     private String Direccion;
+
     private String FechaNacimiento;
-    private String FechaBaja;
-    private String PeliculasAlquiladas;
 
 //Constructor Cliente
     public Cliente(String DNI,String Nombre,int NumSocio,String Direccion,String FechaNacimiento,String FechaBaja,String FechaAlquiladas ){
@@ -18,10 +21,7 @@ public class Cliente {
         this.NumSocio = this.contador;
         this.contador ++;
         this.DNI = DNI;
-        this.FechaBaja = LocalDate.now().toString();
         this.FechaNacimiento = LocalDate.now().toString();
-        this.PeliculasAlquiladas = FechaAlquiladas;
-
     }
 //Getters y Setters
     public int getNumSocio(){
@@ -36,16 +36,13 @@ public class Cliente {
     public String getDni(){
         return this.DNI;
     }
-    public String getFechadeBaja(){
-        return this.FechaBaja;
-    }
     public String FechaNacimiento(){
         return this.FechaNacimiento;
     }
 //Mostrar info cliente
     public String mostrarInfoCliente(){
         
-        String info = String.format("Cliente - NumSocio:%s, Direccion:%s, Nombre:%s, Dni:%s, FechadeBaja:%s, FechadeNacimiento",this.Direccion,this.DNI,this.FechaBaja,this.FechaNacimiento,this.Nombre,this.NumSocio);
+        String info = String.format("Cliente - NumSocio:%s, Direccion:%s, Nombre:%s, Dni:%s,FechadeNacimiento",this.Direccion,this.DNI,this.FechaNacimiento,this.Nombre,this.NumSocio);
             return info ;
     }
 }
--- UD3_Actividad_02_VideoDaw_PEDROARZA/UD3_Actividad_02_VideoDaw/src/Pelicula.java
+++ UD3_Actividad_02_VideoDaw_PEDROARZA/UD3_Actividad_02_VideoDaw/src/Pelicula.java
@@ -1,5 +1,4 @@
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public class Pelicula {
             private static int contador = 0;
@@ -7,10 +6,6 @@ public class Pelicula {
             private String Titulo;
             private String Genero;
             private LocalDate FechaRegistro;
-            private LocalDate FechaBaja;
-            private LocalDateTime FechaAlquiler;
-            private String IsAlquilada;
-
         //Constructor
             public Pelicula(String tipo, int cantidad){
 
@@ -19,9 +14,6 @@ public class Pelicula {
                 this.Titulo = Titulo;
                 this.Genero=Genero;
                 this.FechaRegistro=LocalDate.now();
-                this.FechaBaja=LocalDate.now();
-                this.FechaAlquiler=LocalDateTime.now();
-                this.IsAlquilada=IsAlquilada;
             }
         //Getters ySetters
             public int getcodigo(){
@@ -36,20 +28,11 @@ public class Pelicula {
             public LocalDate getFechaRegistro(){
                 return this.FechaRegistro;
             }
-            public LocalDate getFechaBaja(){
-                return this.FechaBaja;
-            }
-            public LocalDateTime getFechaAlquiler(){
-                return this.FechaAlquiler;
-            }
-            public String getIsAlquilada (){
-                return this.IsAlquilada;
-            }
         //Metodo mostrarInfoPelicula
             
             public String mostrarInfoPelicula(){
 
-                String info = String.format("Pelicula - Titulo:%s, Genero:%s,FechaRegistro:%s,FechaBaja:%s,FechaAlquiler%s",this.Titulo,this.Genero,this.FechaRegistro,this.FechaBaja,this.FechaAlquiler);
+                String info = String.format("Pelicula - Titulo:%s, Genero:%s,FechaRegistro:%s",this.Titulo,this.Genero,this.FechaRegistro);
                         return info;
             }
 
