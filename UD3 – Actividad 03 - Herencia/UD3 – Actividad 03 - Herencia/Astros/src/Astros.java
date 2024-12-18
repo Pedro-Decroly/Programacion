36899dc3237a85c0d5025fab4fc51b1bfebf343d
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2024-12-18 10:02:27 +0100

    no message

---
+++ UD3 – Actividad 03 - Herencia/UD3 – Actividad 03 - Herencia/Astros/src/Astros.java
@@ -0,0 +1,29 @@
+public abstract  class Astros {
+ 
+        private double radioEcuatorial;
+        private double rotacionEje;
+        private double masa;
+        private double temperaturaMedia;
+        private double gravedad;
+
+public double getmasa(){
+    return masa;
+}
+
+public double getrotacionEje(){
+    return rotacionEje;
+}
+
+public double getradioEcuatorial(){
+    return radioEcuatorial;
+}
+
+public double getgravedad(){
+    return gravedad;
+}
+
+
+}
+   
+
+
