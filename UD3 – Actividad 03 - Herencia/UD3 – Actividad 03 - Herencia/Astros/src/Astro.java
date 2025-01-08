b8bc8cfafcfeb5c28ea4b7edfa06d33334ee53cf
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2025-01-08 08:32:41 +0100

    no message

--- UD3 – Actividad 03 - Herencia/UD3 – Actividad 03 - Herencia/Astros/src/Astro.java
+++ UD3 – Actividad 03 - Herencia/UD3 – Actividad 03 - Herencia/Astros/src/Astro.java
@@ -1,28 +1,46 @@
 public abstract class Astro {
+
     protected String nombre;
+    
     protected double radioEcuatorial;
+
     protected double rotacion;
+
     protected double masa;
+
     protected double temperatura;
+
     protected double gravedad;
     
-    public Astro(String nombre, double radioEcuatorial, double rotacion, 
-                 double masa, double temperatura, double gravedad) {
+    public Astro(String nombre, double radioEcuatorial, double rotacion,double masa, double temperatura, double gravedad) {
+
         this.nombre = nombre;
+
         this.radioEcuatorial = radioEcuatorial;
+
         this.rotacion = rotacion;
+
         this.masa = masa;
+
         this.temperatura = temperatura;
+
         this.gravedad = gravedad;
     }
     
     public void muestra() {
+
         System.out.println("Nombre: " + nombre);
+
         System.out.println("Radio: " + radioEcuatorial);
+
         System.out.println("Rotación: " + rotacion);
+
         System.out.println("Masa: " + masa);
+
         System.out.println("Temperatura: " + temperatura);
+
         System.out.println("Gravedad: " + gravedad);
+
     }
 }
    
--- UD3 – Actividad 03 - Herencia/UD3 – Actividad 03 - Herencia/Astros/src/Satelite.java
+++ UD3 – Actividad 03 - Herencia/UD3 – Actividad 03 - Herencia/Astros/src/Satelite.java
@@ -1,12 +1,18 @@
 public class Satelite extends Astro {
+
     private double distanciaPlaneta;
+    
     private Planeta planeta;
     
-    public Satelite(String nombre, double radioEcuatorial, double rotacion, 
-                   double masa, double temperatura, double gravedad, 
-                   double distanciaPlaneta, Planeta planeta) {
+    public Satelite(String nombre, double radioEcuatorial, double rotacion, double masa, double temperatura, double gravedad, 
+                    double distanciaPlaneta, Planeta planeta) {
+
         super(nombre, radioEcuatorial, rotacion, masa, temperatura, gravedad);
+
         this.distanciaPlaneta = distanciaPlaneta;
+
         this.planeta = planeta;
+
     }
+
 }
