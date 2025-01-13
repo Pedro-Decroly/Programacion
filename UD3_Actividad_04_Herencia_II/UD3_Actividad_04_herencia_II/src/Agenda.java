6b75c635def5b091fe19c77f0a03dbf5cb26ffba
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2025-01-13 12:45:31 +0100

    no message

--- UD3_Actividad_04_Herencia_II/UD3_Actividad_04_herencia_II/src/Agenda.java
+++ UD3_Actividad_04_Herencia_II/UD3_Actividad_04_herencia_II/src/Agenda.java
@@ -1,17 +1,29 @@
 public class Agenda extends Contacto {
 
-private String buscaContacto;
-private String listarContactos;
-private String existeContacto;
+    private String buscaContacto;
+    private String listarContactos;
+    private String existeContacto;
 
-public Agenda(String nombre, String telefono, String buscaContacto, String listarContactos, String existeContacto) {
-    
-    super(nombre, telefono);
+    public Agenda(String nombre, String telefono, String buscaContacto, String listarContactos, String existeContacto) {
+        
+        super(nombre, telefono);
 
-    this.buscaContacto = buscaContacto;
-    this.listarContactos = listarContactos;
-    this.existeContacto = existeContacto;
-}
+        this.buscaContacto = buscaContacto;
+        this.listarContactos = listarContactos;
+        this.existeContacto = existeContacto;
+    }
+
+    public void buscaContacto(String buscaContacto) {
+        this.buscaContacto = buscaContacto;
+    }
+
+    public void listarContactos(String listarContactos) {
+        this.listarContactos = listarContactos;
+    }
+
+    public void existeContacto(String existeContacto) {
+        this.existeContacto = existeContacto;
+    }
 
 
 }
