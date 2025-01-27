49b3d4bb3cc21360db019fe7d3359a0dc0a3267c
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2025-01-27 12:48:05 +0100

    no message

---
+++ UD4_Actividad_02_Colecciones/Proyecto Taller/src/Coche.java
@@ -0,0 +1,42 @@
+public class Coche {
+    
+        private String marca;
+        private String color;
+
+// Constructor 
+    public Coche(String color, String marca) {
+        this.color = color;
+        this.marca = marca;
+    }
+
+// Metodo Getters
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getcolor() {
+        return color;
+    }
+
+//Metodos Setters
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+//Metodo To String 
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Coche{");
+        sb.append("marca=").append(marca);
+        sb.append(", color=").append(color);
+        sb.append('}');
+        return sb.toString();
+    }
+     
+}
---
+++ UD4_Actividad_02_Colecciones/Proyecto Taller/src/Taller.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+
+import java.util.HashMap;
+
+    public class Taller {
+
+    Map<String, Coche> Coches = new HashMap<String, Coche>();
+
+    public void añadeElemento(String matricula, String marca, String color){
+
+    Coche c1 = new Coche("color", "marca");
+
+    
+
+    }
+
+}
