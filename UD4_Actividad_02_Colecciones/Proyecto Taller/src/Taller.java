b865a39884623ec6321efee5a73133c4c583bc4d
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2025-01-27 12:53:29 +0100

    no message

--- UD4_Actividad_02_Colecciones/Proyecto Taller/src/Taller.java
+++ UD4_Actividad_02_Colecciones/Proyecto Taller/src/Taller.java
@@ -2,15 +2,14 @@ import java.util.Map;
 
 import java.util.HashMap;
 
-    public class Taller {
+public class Taller {
 
     Map<String, Coche> Coches = new HashMap<String, Coche>();
 
     public void añadeElemento(String matricula, String marca, String color){
 
-    Coche c1 = new Coche("color", "marca");
-
-    
+        Coche c1 = new Coche("color", "marca");
+        
 
     }
 
