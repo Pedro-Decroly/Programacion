2a8afef51011dfb678ffdaad2bade0c6b37026f4
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2025-02-05 09:52:14 +0100

    no message

---
+++ UD4 – Actividad 02 – Colecciones y Estructuras de Datos/Colecciones y Estructuras de Datos/src/App.java
@@ -0,0 +1,65 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws InputMismatchException  {
+        System.out.println("Hello, World!");
+
+//Ejercicio1
+
+Scanner rc = new Scanner (System.in);
+
+try {
+    System.out.println("INTODUCE UN NUMERO ENTERO");
+    int A = rc.nextInt();
+} catch (InputMismatchException A) {
+    System.out.println("Vuelve a intentarlo");
+}
+
+//Ejercicio2
+
+    try{
+        System.out.println("Introduce un numero");
+        int A = rc.nextInt();
+
+        System.out.println("Introduce un numero");
+        int B = rc.nextInt();
+
+        System.out.println(A/B);
+    }
+    catch(InputMismatchException A){
+        System.out.println("Valor erroneo");
+    }
+    catch(ArithmeticException B){
+        System.out.println("Calculo erroneo");
+
+    }
+//Ejercicio3
+
+for(double i = 0; i < 5; i++) {
+    try {
+        System.out.print("Introduce un número: ");
+        int A = rc.nextInt();
+    } catch (InputMismatchException e) {
+        System.out.println("Error - Introduce un número válido");
+        rc.nextLine();
+        i--;
+    }
+}
+//Ejercicio4
+    try {
+        
+    } 
+    catch (InputMismatchException e) {
+    }
+
+
+//Ejercicio5
+
+
+//Ejercicio6
+
+
+    }
+}
