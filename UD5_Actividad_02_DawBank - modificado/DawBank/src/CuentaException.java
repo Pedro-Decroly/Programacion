fe9bed22036ea9f46633e9aa7ae8804172d6b085
COMPU-S037\daw1 <pedroarza.03@gmail.com>
2025-02-14 09:51:35 +0100

    no message

--- UD5_Actividad_02_DawBank - modificado/DawBank/src/CuentaException.java
+++ UD5_Actividad_02_DawBank - modificado/DawBank/src/CuentaException.java
@@ -1,3 +1,12 @@
-public class CuentaException {
+public class CuentaException extends Exception {
+
+    public static void main(String[] args) {
+    
+        try {
+            
+        } catch (Exception e) {
+        } finally {
+        }
+    }
 
 }
