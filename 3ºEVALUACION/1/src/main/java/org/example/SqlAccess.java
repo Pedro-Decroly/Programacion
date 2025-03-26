fb6995c292d400c5d3a8ae9c5ea89d9e118c36e5
COMPU-S033\daw1 <kikstark>
2025-03-26 11:35:12 +0100

    no message

--- 3ºEVALUACION/1/src/main/java/org/example/SqlAccess.java
+++ 3ºEVALUACION/1/src/main/java/org/example/SqlAccess.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class SqlAccess {
+
+    public List<String> getChurreriaName(){
+        List<String> churreria = new LinkedList<>();
+        String getChNames = "SELECT churreria.NOMBRE FROM churreria";
+
+
+    }
+
 }
